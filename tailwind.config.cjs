624f446cab8c667181b8e66c0a0b844ccf25d6a0
Abderrahim Tantaoui <abderrahim.tantaoui@viziosense.com>
2022-11-08 21:44:02 +0100

    added ubuntu font as default

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                sans: ["Ubuntu", ...defaultTheme.fontFamily.sans],
+            },
+        },
     },
     plugins: [],
     tailwindConfig: "./styles/tailwind.config.js",
