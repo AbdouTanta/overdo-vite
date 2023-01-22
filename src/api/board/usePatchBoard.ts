ddcb18e2d606fcc4607ea219819e1060972c23a7
Abderrahim Tantaoui <abdoutanta@gmail.com>
2023-01-22 20:40:45 +0100

    created a custom hook for board edit mutation

--- src/api/board/usePatchBoard.ts
+++ src/api/board/usePatchBoard.ts
@@ -1,24 +1,28 @@
 import { useMutation, UseMutationOptions } from '@tanstack/react-query';
-import { IBoard } from '../../types/IBoard';
+import { AxiosError } from 'axios';
+import { EditBoardDTO, IBoard } from '../../types/IBoard';
 import client from '../common/client';
-
-function patchBoard(boardId: string) {
-  return client.patch(`boards/${boardId}`).then((res) => {
-    return res.data.lists;
-  });
-}
 
 type Response = IBoard;
 
-type UsePatchBoardOptions = {
-  config?: UseMutationOptions<Response>;
+type PatchBoardOptions = {
   boardId: string;
+  payload: EditBoardDTO;
 };
 
-export function usePatchBoard({ config, boardId }: UsePatchBoardOptions) {
-  return useMutation({
-    mutationKey: ['board', boardId],
-    mutationFn: () => patchBoard(boardId),
+function patchBoard({
+  boardId,
+  payload,
+}: PatchBoardOptions): Promise<Response> {
+  return client.patch(`boards/${boardId}`, payload).then((res) => {
+    return res.data;
+  });
+}
+
+export function usePatchBoard(
+  config: UseMutationOptions<Response, AxiosError, PatchBoardOptions>
+) {
+  return useMutation<IBoard, AxiosError, PatchBoardOptions>(patchBoard, {
     ...config,
   });
 }
