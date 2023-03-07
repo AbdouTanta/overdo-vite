ded76e1b1688555ac6b0a95f0acd0f7203831858
Abderrahim Tantaoui <abdoutanta@gmail.com>
2023-03-07 20:47:37 +0100

    remove animations

--- src/components/list/ListGrid.tsx
+++ src/components/list/ListGrid.tsx
@@ -1,13 +1,6 @@
 /* eslint-disable react/jsx-no-bind */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
-import {
-  DndContext,
-  PointerSensor,
-  useSensor,
-  useSensors,
-  closestCenter,
-  useDroppable,
-} from '@dnd-kit/core';
+import { DndContext, closestCenter, useDroppable } from '@dnd-kit/core';
 import {
   restrictToHorizontalAxis,
   restrictToParentElement,
@@ -26,9 +19,6 @@ import ListPlaceholder from './ListPlaceholder';
 function ListGrid({ board }: { board: IBoard }) {
   const { data: lists, isLoading } = useGetBoard({ boardId: board.id });
 
-  // const sensors = useSensors(
-  //   useSensor(PointerSensor, { activationConstraint: { distance: 8 } })
-  // );
   const { setNodeRef } = useDroppable({ id: 'listgrid' });
 
   function handleDragEnd() {}
@@ -39,17 +29,11 @@ function ListGrid({ board }: { board: IBoard }) {
 
   return (
     <DndContext
-      // sensors={sensors}
       modifiers={[restrictToHorizontalAxis, restrictToParentElement]}
       collisionDetection={closestCenter}
       onDragEnd={handleDragEnd}
     >
-      <motion.div
-        initial={{ opacity: 0, y: 100 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ ease: 'easeOut', duration: 0.5 }}
-        className="flex flex-row"
-      >
+      <div className="flex flex-row">
         {lists.length !== 0 && (
           <div
             className="scrollbar flex items-start gap-4 overflow-x-auto pb-4 pr-4"
@@ -68,7 +52,7 @@ function ListGrid({ board }: { board: IBoard }) {
         <div className="flex flex-col gap-4">
           <ListPlaceholder />
         </div>
-      </motion.div>
+      </div>
     </DndContext>
   );
 }
